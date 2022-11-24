690d24d6e44be2330c7f7745b17c90c3f858af6e
chpolina <polinaluk08@mail.ru>
2022-11-24 21:37:49 +0300

    прилож.скоп

--- webinar_7_1.py
+++ webinar_7_1.py
@@ -1,7 +1,9 @@
 from tkinter import *
 
 def button_1():
-    name_result.setvar()
+    name_result.config(text=str(name.get()))
+    lastname_result.config(text=str(lastname.get()))
+    tel_result.config(text=str(tel.get()))
 test=0
 root=Tk()
 root.title("приложение")
@@ -12,18 +14,11 @@ lastname=Entry(root, width=15,bg="gray",fg="cyan",font="consolas")
 lastname.pack()
 tel=Entry(root, width=15,bg="gray",fg="cyan",font="consolas")
 tel.pack()
-
-name_result=Entry(root, width=15,bg="gray",fg="cyan",font="consolas")
-name_result.pack()
-lastname_result=Entry(root, width=15,bg="gray",fg="cyan",font="consolas")
-lastname_result.pack()
-tel_result=Entry(root, width=15,bg="gray",fg="cyan",font="consolas")
-tel_result.pack()
-
 Button(root, text="скопировать",width=15,height=2,bg="white",command=button_1).pack()
-
-
-
-l=Label(root,width=15,bg="gray",fg="cyan",font="consolas")
-l.pack()
+name_result=Label(root,width=15,bg="gray",fg="cyan",font="consolas")
+name_result.pack()
+lastname_result=Label(root,width=15,bg="gray",fg="cyan",font="consolas")
+lastname_result.pack()
+tel_result=Label(root,width=15,bg="gray",fg="cyan",font="consolas")
+tel_result.pack()
 root.mainloop()
